bc2f254f318aababc5178b2405ccce0cf515d2c0
Ethan Willinger <45436341+SailorMercuryChan@users.noreply.github.com>
2018-11-28 14:21:15 -0600

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+
+    {
+    public GameObject player;
+    public Enemy dick;
+
+        private rigidbody rb;
+        
+    [SerializeField] private int score;
+
+    private void Start()t
+    {
+        
+
+    }
+
+    private void Update()
+    {
+        if (left arrrow is pressed)
+
+        translate.
+    }
+
+}
